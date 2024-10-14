fef14f6497bc97f5db1ffd0295445482e33266ee
Autumn Eckley <eckley.autumn@gmail.com>
2024-10-14 15:35:27 -0600

    You can choose a hand

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 {
   private static void Main()
   {
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Rock, Paper, Scissors!");
+    string userHand = ChooseHand();
+    Console.WriteLine(userHand);
+  }
+
+  static string ChooseHand()
+  {
+    Console.WriteLine("Choose a Hand");
+    Console.WriteLine("1. Rock");
+    Console.WriteLine("2. Paper");
+    Console.WriteLine("3. Scissors");
+    string userInput = Console.ReadLine();
+
+    if (userInput == "1")
+    {
+      return "You chose Rock 🪨";
+    }
+    if (userInput == "2")
+    {
+      return "You chose Paper 📄";
+    }
+    if (userInput == "3")
+    {
+      return "You chose Scissors✂️";
+    }
+    return "Pick a number";
   }
 }
